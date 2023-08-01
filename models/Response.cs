81ad7413ca11de7a4461da18c2f84699df0e9f9d
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-08-01 12:17:44 +0200

    upload project

---
+++ models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthAPi.models
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ models/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthAPi.models
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
